63b5c4e2a09aac88c8c21d77fcfdef745bf99057
Alex Chang <75132115+alexch1234@users.noreply.github.com>
2023-12-06 19:05:47 +0000

    feat: Complete rock, paper, scissors game

---
+++ scripts.js
@@ -0,0 +1,57 @@
+const POSSIBLE_CHOICES = [ "Rock", "Paper", "Scissors" ];
+
+function getComputerChoice() {
+    
+    randomNumber = Math.floor(Math.random() * POSSIBLE_CHOICES.length);
+    return POSSIBLE_CHOICES[randomNumber];
+}
+
+function isPlayerChoiceValid(playerChoice) {
+    return POSSIBLE_CHOICES.filter(choice => choice.toLowerCase() == playerChoice).length != 0;
+}
+
+function playRound(playerChoice, computerChoice) {
+    outcomes = {
+        "rock": "scissors",
+        "paper": "rock",
+        "scissors": "paper" 
+    }
+
+    for (let winner in outcomes) {
+        if (playerChoice == winner) {
+            return computerChoice == outcomes[winner] ? "Player" : "Computer";
+        }
+    }
+}
+
+function playGame() {
+    playerScore = 0;
+    computerScore = 0;
+    while(playerScore < 3 && computerScore < 3) {
+        playerChoice = prompt(`Score: ${playerScore}-${computerScore} Please choose Rock, Paper, or Scissors: `).toLowerCase();
+        computerChoice = getComputerChoice().toLowerCase();
+        while (!isPlayerChoiceValid(playerChoice)) {
+            playerChoice = prompt("Invalid choice. Please choose Rock, Paper, or Scissors: ").toLowerCase();
+        }
+        computerChoice = getComputerChoice().toLowerCase();
+        
+        while(playerChoice == computerChoice || !isPlayerChoiceValid(playerChoice)) {
+            computerChoice = getComputerChoice().toLowerCase();
+            if (!isPlayerChoiceValid(playerChoice)) {
+                playerChoice = prompt("Invalid choice. Please choose Rock, Paper, or Scissors: ").toLowerCase();
+            } else {
+                playerChoice = prompt("It's a Draw! Please choose again: ");
+            }
+        }
+
+        if (playRound(playerChoice, computerChoice) == "Player") {
+            playerScore++;
+        } else {
+            computerScore++;
+        }
+    }
+
+    console.log(`${playerScore === 3 ? "Player" : "Computer"} wins ${playerScore}-${computerScore}!`);
+}
+
+playGame();
