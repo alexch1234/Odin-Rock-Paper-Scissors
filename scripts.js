a6c85043434e13dc5909b9126991fee8820fb0a0
Alex Chang <75132115+alexch1234@users.noreply.github.com>
2023-12-09 17:34:51 +0000

    feat: Change game to work with simple UI
    - Add buttons for rock, paper, and scissors for user to choose.
    - Add a display to show the current scores

--- scripts.js
+++ scripts.js
@@ -1,13 +1,52 @@
-const POSSIBLE_CHOICES = [ "Rock", "Paper", "Scissors" ];
+const POSSIBLE_CHOICES = [ "rock", "paper", "scissors" ];
 
-function getComputerChoice() {
-    
-    randomNumber = Math.floor(Math.random() * POSSIBLE_CHOICES.length);
-    return POSSIBLE_CHOICES[randomNumber];
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+const scoresDisplay = document.querySelector('#scores');
+
+scoresDisplay.textContent = getScores(playerScore, computerScore);
+
+buttons.forEach(button => {
+    button.addEventListener('click', handleButtonClick);
+});
+
+function handleButtonClick(event) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = event.target.id;
+    playRound(playerChoice, computerChoice);
+    scoresDisplay.textContent = getScores(playerScore, computerScore);
+    if (playerScore === 5) {
+        finishGame(scoresDisplay, 'Player');
+    }
+    if (computerScore === 5) {
+        finishGame(scoresDisplay, 'Computer');
+    }
 }
 
-function isPlayerChoiceValid(playerChoice) {
-    return POSSIBLE_CHOICES.filter(choice => choice.toLowerCase() == playerChoice).length != 0;
+function finishGame(display, winner) {
+    buttons.forEach(button => button.disabled = true);
+    display.textContent = `${winner} wins!`;
+    let resetButton = document.createElement('button');
+    resetButton.textContent = 'Restart game';
+    resetButton.addEventListener('click', () => {
+        playerScore = 0;
+        computerScore = 0;
+        buttons.forEach(button => button.disabled = false);
+        display.textContent = getScores(playerScore, computerScore);
+        document.querySelector('body').removeChild(resetButton);
+    });
+    document.querySelector('body').appendChild(resetButton);
+}
+
+function getScores(playerScore, computerScore) {
+    return `Player Score: ${playerScore}\nComputer Score: ${computerScore}\n`;
+}
+
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * POSSIBLE_CHOICES.length);
+    return POSSIBLE_CHOICES[randomNumber];
 }
 
 function playRound(playerChoice, computerChoice) {
@@ -18,45 +57,18 @@ function playRound(playerChoice, computerChoice) {
     }
 
     for (let winner in outcomes) {
-        if (playerChoice == winner) {
-            if (computerChoice == outcomes[winner]) {
+        if (playerChoice === winner) {
+            if (computerChoice === outcomes[winner]) {
                 console.log(`Player wins! ${playerChoice} beats ${computerChoice}`)
-                return "Player";
+                playerScore++;
+            } else if (playerChoice === computerChoice) {
+                console.log('draw!');
+                playerScore++;
+                computerScore++;
             } else {
-                console.log(`Computer wins! ${computerChoice} beats ${playerChoice}`)
-                return "Computer";
+                console.log(`Computer wins! ${computerChoice} beats ${playerChoice}`);
+                computerScore++;
             }
         }
     }
 }
-
-function playGame() {
-    playerScore = 0;
-    computerScore = 0;
-    while(playerScore < 3 && computerScore < 3) {
-        playerChoice = prompt(`Score: ${playerScore}-${computerScore} Please choose Rock, Paper, or Scissors: `).toLowerCase();
-        while (!isPlayerChoiceValid(playerChoice)) {
-            playerChoice = prompt("Invalid choice. Please choose Rock, Paper, or Scissors: ").toLowerCase();
-        }
-        computerChoice = getComputerChoice().toLowerCase();
-        
-        while(playerChoice == computerChoice || !isPlayerChoiceValid(playerChoice)) {
-            computerChoice = getComputerChoice().toLowerCase();
-            if (!isPlayerChoiceValid(playerChoice)) {
-                playerChoice = prompt("Invalid choice. Please choose Rock, Paper, or Scissors: ").toLowerCase();
-            } else {
-                playerChoice = prompt("It's a Draw! Please choose again: ");
-            }
-        }
-
-        if (playRound(playerChoice, computerChoice) == "Player") {
-            playerScore++;
-        } else {
-            computerScore++;
-        }
-    }
-
-    console.log(`${playerScore === 3 ? "Player" : "Computer"} wins ${playerScore}-${computerScore}!`);
-}
-
-playGame();
